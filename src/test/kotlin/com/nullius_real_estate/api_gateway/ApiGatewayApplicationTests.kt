4ec0c8ca19efaf0b6c3fdc56d64b54f9f56df32c
KevOrtega <kevortega.dev@gmail.com>
2025-02-13 17:54:50 -0300

    chore: add setup and connection to Eureka server as client

---
+++ src/test/kotlin/com/nullius_real_estate/api_gateway/ApiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nullius_real_estate.api_gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
