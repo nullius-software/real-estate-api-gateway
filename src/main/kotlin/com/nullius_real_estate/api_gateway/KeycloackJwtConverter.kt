5b56861975a1f9dcc75020a64220aa1f89b9729e
KevOrtega <kevortega.dev@gmail.com>
2025-02-20 15:45:27 -0300

    refactor: make app reactive and implement keycloack converter on configuration

--- src/main/kotlin/com/nullius_real_estate/api_gateway/KeycloackJwtConverter.kt
+++ src/main/kotlin/com/nullius_real_estate/api_gateway/KeycloackJwtConverter.kt
@@ -1,27 +1,29 @@
 package com.nullius_real_estate.api_gateway
 
 import org.springframework.core.convert.converter.Converter
-import org.springframework.lang.NonNull
 import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
+import reactor.core.publisher.Mono
 import java.util.stream.Collectors.toSet
 import java.util.stream.Stream
 
-class KeycloackJwtConverter : Converter<Jwt, AbstractAuthenticationToken> {
-    override fun convert(@NonNull source: Jwt): AbstractAuthenticationToken {
-        return JwtAuthenticationToken(
-            source,
-            Stream.concat(
-                JwtGrantedAuthoritiesConverter().convert(source)!!.stream(),
-                extractResourcesRoles(source).stream()
-            ).collect(
-                toSet()
-            ),
-            source.subject
+class KeycloackJwtConverter : Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+    override fun convert(source: Jwt): Mono<AbstractAuthenticationToken> {
+        val authorities = Stream.concat(
+            JwtGrantedAuthoritiesConverter().convert(source)?.stream() ?: Stream.empty(),
+            extractResourcesRoles(source).stream()
+        ).collect(toSet())
+
+        return Mono.just(
+            JwtAuthenticationToken(
+                source,
+                authorities,
+                source.subject
+            )
         )
     }
 
--- src/main/kotlin/com/nullius_real_estate/api_gateway/SecurityConfig.kt
+++ src/main/kotlin/com/nullius_real_estate/api_gateway/SecurityConfig.kt
@@ -3,37 +3,36 @@ package com.nullius_real_estate.api_gateway
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
-import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.jwt.JwtDecoders
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
-import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true)
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 class SecurityConfig {
 
     @Bean
     @Throws(Exception::class)
-    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+    fun securityFilterChain(httpSecurity: ServerHttpSecurity): SecurityWebFilterChain {
         httpSecurity
             .csrf { it.disable() }
-            .authorizeHttpRequests { it.anyRequest().authenticated() }
             .cors(Customizer.withDefaults())
+            .authorizeExchange {
+                it.anyExchange().authenticated()
+            }
             .oauth2ResourceServer {
-                it.jwt { }
+                it.jwt { jwt ->
+                    jwt.jwtAuthenticationConverter(keycloakJwtConverter())
+                }
             }
 
         return httpSecurity.build()
     }
 
     @Bean
-    fun jwtDecoder(): JwtDecoder {
-        val jwkSetUri = "http://localhost:9090/realms/nullius-real-estate-realm/protocol/openid-connect/certs"
-        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build()
+    fun keycloakJwtConverter(): KeycloackJwtConverter {
+        return KeycloackJwtConverter()
     }
 }
